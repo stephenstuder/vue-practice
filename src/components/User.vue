5b6ec25d5c665f4be7c9f361e69695509283daaf
Stephen Studer <stephenkstuder@gmail.com>
2020-07-26 17:37:13 -0700

    initial commit of practice project

---
+++ src/components/User.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <p>Name is {{ name }}</p>
+    <button @click="changeName">Change My Name</button>
+
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail
+          :name="name"
+          @nameWasReset="name = $event"
+          :resetFn="resetName"
+          :userAge="age"
+        ></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit
+          :userAge="age"
+          @ageWasEdited="age = $event"
+        ></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  data() {
+    return {
+      name: "Stephen",
+      age: 27,
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "Makenna";
+    },
+    resetName() {
+      this.name = "Stephen";
+    },
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+    <p>User Age: {{ userAge }}</p>
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetFn">Reset Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    resetFn: Function,
+    userAge: Number,
+  },
+  methods: {
+    switchName() {
+      return this.name.split("").reverse().join("");
+    },
+    resetName() {
+      this.name = "Stephen";
+      this.$emit("nameWasReset", this.name);
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
