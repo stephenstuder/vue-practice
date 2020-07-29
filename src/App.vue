cab8f7758b7352c3de91635d95e7c5cba35ad38c
Stephen Studer <stephenkstuder@gmail.com>
2020-07-28 20:14:08 -0700

    starter code for using and creating directives

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12">
-                <app-user></app-user>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives</h1>
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
-
     export default {
-        components: {
-            appUser: User
-        }
     }
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
+
 </style>
