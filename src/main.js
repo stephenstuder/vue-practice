35892b478e3d953cec1c29bb721064d080282710
Stephen Studer <stephenkstuder@gmail.com>
2020-07-28 20:45:52 -0700

    creating a custom directive for highlighting text

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    //el.style.backgroundColor = "green";
+    el.style.backgroundColor = binding.value;
+  },
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  render: (h) => h(App),
+});
