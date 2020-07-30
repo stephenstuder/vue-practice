c1dda7c544ad669ddc8c99d57b71d5ab795816ed
Stephen Studer <stephenkstuder@gmail.com>
2020-07-29 19:12:16 -0700

    globally registered filter

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
-        <p>{{text | toUppercase}}</p>
+        <p>{{text | toUpperCase | toLowerCase}}</p>
+        <p>{{text | toUpperCase}}</p>
       </div>
     </div>
   </div>
@@ -19,7 +20,7 @@ export default {
     };
   },
   filters: {
-    toUppercase(value) {
+    toUpperCase(value) {
       return value.toUpperCase();
     },
   },
