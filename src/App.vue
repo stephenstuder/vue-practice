42db7ebb5845eb19515a60f7244b2c6ec6490a3e
Stephen Studer <stephenkstuder@gmail.com>
2020-07-29 18:22:14 -0700

    creation of a local filter

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{text | toUppercase}}</p>
+      </div>
     </div>
+  </div>
 </template>
 
+// filter is used in instances where you want to change the display of data without actually changing the data
+
 <script>
-    export default {
-    }
+export default {
+  data() {
+    return {
+      text: "Hello There!",
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
