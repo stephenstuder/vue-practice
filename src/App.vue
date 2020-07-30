80ec8c67eebb5545d6884253ddfc26ca20ba171b
Stephen Studer <stephenkstuder@gmail.com>
2020-07-30 16:18:07 -0700

    use computed property as a performant filter

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
         <h1>Filters & Mixins</h1>
         <p>{{text | toUpperCase | toLowerCase}}</p>
         <p>{{text | toUpperCase}}</p>
+        <hr />
+        <input v-model="filterText" type="text" />
+        <ul>
+          <li v-for="fruit in filteredFruits" :key="fruit.id">{{fruit}}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -17,11 +22,20 @@ export default {
   data() {
     return {
       text: "Hello There!",
+      fruits: ["apple", "banana", "mango", "melon"],
+      filterText: "",
     };
   },
   filters: {
     toUpperCase(value) {
       return value.toUpperCase();
+    },
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText);
+      });
     },
   },
 };
