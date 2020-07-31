b969234fc1d5580c1912ed641b45a0125a755f59
Stephen Studer <stephenkstuder@gmail.com>
2020-07-30 19:48:28 -0700

    starter code for animations and transitions

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,24 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>
-        <p>{{text | toUpperCase | toLowerCase}}</p>
-        <p>{{text | toUpperCase}}</p>
-        <hr />
-        <button @click="fruits.push('berries')">Add New Item</button>
-        <input v-model="filterText" type="text" />
-        <ul>
-          <li v-for="fruit in filteredFruits" :key="fruit.id">{{fruit}}</li>
-        </ul>
-        <hr />
-        <app-list></app-list>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
-// filter is used in instances where you want to change the display of data without actually changing the data
-// component always has the last word, mixin is executed first
 <script>
-import List from "./List.vue";
-import { fruitMixin } from "./fruitMixin.js";
-export default {
-  data() {
-    return {
-      text: "Hello There!",
-    };
-  },
-  filters: {
-    toUpperCase(value) {
-      return value.toUpperCase();
-    },
-  },
-  mixins: [fruitMixin],
-  components: {
-    appList: List,
-  },
-};
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
 </script>
 
 <style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
-import Vue from "vue";
-import App from "./App.vue";
-
-Vue.filter("toLowerCase", (value) => {
-  return value.toLowerCase();
-});
-
-Vue.mixin({
-  created() {
-    console.log("Global mixin created hook");
-  },
-});
+import Vue from 'vue'
+import App from './App.vue'
 
 new Vue({
-  el: "#app",
-  render: (h) => h(App),
-});
+  el: '#app',
+  render: h => h(App)
+})
