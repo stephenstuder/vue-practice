b7f26342fe8a0d776b553a1522ade784c3885d34
Stephen Studer <stephenkstuder@gmail.com>
2020-07-30 20:57:10 -0700

    mixing animation and transitions

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@
         <transition name="fade">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
+        <transition name="slide" type="transition" appear>
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -37,5 +40,35 @@ export default {
 .fade-leave-active {
   transition: opacity 1s;
   opacity: 0;
+}
+.slide-enter {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 1s;
+}
+.slide-leave {
+}
+.slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  transition: opacity 1s;
+  opacity: 0;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
 }
 </style>
