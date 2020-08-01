4c3383bc0cdf2875943809ef7d6f2681a5d20926
Stephen Studer <stephenkstuder@gmail.com>
2020-07-31 20:41:46 -0700

    using if else to render seperate elements using a key and the mode attribute

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,12 @@
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <br />
         <br />
-        <transition :name="alertAnimation">
+        <!-- <transition :name="alertAnimation">
           <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>-->
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+          <div class="alert alert-warning" v-else key="warning">This is some info</div>
         </transition>
       </div>
     </div>
