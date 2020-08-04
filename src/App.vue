f9e7cd88bbf4e50b5f258bfa063f3bf1b52ba0a0
Stephen Studer <stephenkstuder@gmail.com>
2020-08-03 19:18:25 -0700

    animations made entirely through javascript

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
           @leave-cancelled="leaveCancelled"
           :css="false"
         >
-          <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load"></div>
+          <div style="width: 300px; height: 100px; background-color: lightgreen;" v-if="load"></div>
         </transition>
       </div>
     </div>
@@ -55,6 +55,8 @@ export default {
   methods: {
     beforeEnter(el) {
       console.log("beforeEnter");
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + "px";
     },
 
     enter(el, done) {
@@ -77,10 +79,20 @@ export default {
     },
     beforeLeave(el) {
       console.log("beforeLeave");
+      this.elementWidth = 300;
+      el.style.width = this.elementWidth + "px";
     },
     leave(el, done) {
       console.log("leave");
-      done();
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth - round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
     afterLeave(el) {
       console.log("afterLeave");
