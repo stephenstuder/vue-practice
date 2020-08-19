c62cd90dc4712887020eca5294c1f41cdfa31537
Stephen Studer <stephenkstuder@gmail.com>
2020-08-18 20:36:22 -0700

    vue routing

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-
+                <h1>Routing</h1>
             </div>
         </div>
     </div>
@@ -11,14 +10,8 @@
 
 <script>
     export default {
-        data() {
-            return {
-
-            }
-        }
     }
 </script>
 
 <style>
-
 </style>
